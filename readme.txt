5943151255a65de9fc85d4a03ff1a173af30588c
sammy <samroque959@gmail.com>
2026-03-22 15:03:23 +0800

    add v2

---
+++ readme.txt
@@ -0,0 +1,8 @@
+v1.
+(ika butang ani marj)
+
+
+v1.2
+Changes all file into php 
+added php functions like session, login, and register
+
---
+++ try.php
@@ -0,0 +1,44 @@
+<?php 
+include("connect.php");
+?>
+
+<!doctype html>
+
+<head>
+    <meta charset ="UTF-8"> 
+    <meta name ="descrition" content ="This is not an awesome website"> <!-- i jst remember how to comment now owwowoowwoowwo lets go!-->
+    <link rel ="stylesheet" href = "style.css">
+    <meta name="viewport" content="width=device-width">
+    <title>practisto nigoto niga?</title>
+
+</head>
+
+<body>
+
+
+<h1>wassup </h1>
+<header> 
+
+
+
+</header>
+
+
+
+
+
+
+<footer> <!--pinaka ubos -->
+
+
+
+
+</footer>
+
+
+
+
+
+</body>
+
+</html> 
